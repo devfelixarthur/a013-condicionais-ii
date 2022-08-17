a337463654c12c0e9b55eb6129f99394131a1300
devfelixarthur <de.felixarthur@gmail.com>
2022-08-17 18:57:12 -0300

    EXERCICIOS CONDICIONAIS

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -10,6 +10,6 @@ if(num){
   
 // Questão 02
   if(num % 2 === 0 || num % 3 === 0) {
-    console.log(num%2===0? 'Esse número é divisível por 2.':'Esse número é divisível por 3.')
+    console.log(num%2===0? 'Esse número é divisível por 2.':'')
     console.log(num%3===0? 'Esse número é divisível por 3.':'')
   }
